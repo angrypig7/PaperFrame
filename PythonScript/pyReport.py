778bd42c48ad7499d56c35d2e6e4d57b68e280a8
angrypig7 <angrypig777@gmail.com>
2018-02-20 12:34:38 +0900

    removed excessive lines

--- PythonScript/pyReport.py
+++ PythonScript/pyReport.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import time
+import string
 
 def upload(serverno, localip, thermal, freq):
         serverno = str(serverno)
@@ -15,42 +16,34 @@ def upload(serverno, localip, thermal, freq):
                 "freq": freq
         }
         response = requests.post(URL, data=data, timeout=10, verify=True)
-        print()
+        
+        print("==============================")
+        print("LOCALIP : " + str(localip))
+        print("==============================")
+        print("SERVERNO : " + str(serverno))
         print("LOCALIP : " + str(localip))
         print("THERMAL : " + str(thermal))
-        print("SERVERNO : " + str(serverno))
+        print("CPUFREQ : " + str(freq))
         print("RESPONSE : " + str(response))
-        print()
-        return
+        print("==============================")
 
-count = 1
+        return response
 
-while 1:
-        serverno = 1
-        localip = os.popen("ifconfig").read()
-        thermal = 123
-#        thermal = os.popen("awk '{printf \"%3.1f\n\", $1/1000}' /sys/class/thermal/thermal_zone0/temp && awk '{printf \"%3.1f\n\", $1/1000}' /sys/class/thermal/thermal_zone1/temp && awk '{printf \"%3.1f\n\", $1/1000}' /sys/class/thermal/thermal_zone2/temp && awk '{printf \"%3.1f\n\", $1/1000}' /sys/class/thermal/thermal_zone3/temp && awk '{printf \"%3.1f\n\", $1/1000}' /sys/class/thermal/thermal_zone4/temp").read()
-        freq = os.popen("cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu1/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu2/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu3/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu5/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu6/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu7/cpufreq/cpuinfo_cur_freq").read()
-        
-        serverno = str(serverno)
-        localip = str(localip)
-        thermal = str(thermal)
-        freq = str(freq)
-        upload(serverno, localip, thermal, freq)
+serverno = 1
+localip = os.popen("ifconfig").read()
+thermal = 123456
+# thermal = os.popen("awk '{printf \"%3.1f\n\", $1/1000}' /sys/class/thermal/thermal_zone0/temp && awk '{printf \"%3.1f\n\", $1/1000}' /sys/class/thermal/thermal_zone1/temp && awk '{printf \"%3.1f\n\", $1/1000}' /sys/class/thermal/thermal_zone2/temp && awk '{printf \"%3.1f\n\", $1/1000}' /sys/class/thermal/thermal_zone3/temp && awk '{printf \"%3.1f\n\", $1/1000}' /sys/class/thermal/thermal_zone4/temp").read()
+freq = os.popen("cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu1/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu2/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu3/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu4/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu5/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu6/cpufreq/cpuinfo_cur_freq && cat /sys/devices/system/cpu/cpu7/cpufreq/cpuinfo_cur_freq").read()
 
-        iplength = len(localip)
-        tlength = len(thermal)
-        if(iplength>5 and tlength>1):
-                print()
+serverno = str(serverno)
+localip = str(localip)
+thermal = str(thermal)
+freq = str(freq)
+iplength = len(localip)
+tlength = len(thermal)
+frlength = len(freq)
+
+if(iplength>5 and tlength>5 and frlength>5):
+        response = str(upload(serverno, localip, thermal, freq))
+        if(str("<Response [200]>") == str(response)):
                 print("SUCCESS")
-                print()
-                count += 100
-        else:
-                count+=1
-                print()
-                print("ERROR")
-                print()
-        if count>2 and count<20:
-                upload(localip, "ERROR1", "ERROR2")
-                upload("ERROR", "ERROR2", "ERROR2")
-        break
